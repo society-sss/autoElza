926eb6e9a58baa77344bd4cddce890b058e12651
JalalovElzar <qwwgcc491@gmail.com>
2025-01-28 20:05:51 +0600

    приложение auto elza

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
